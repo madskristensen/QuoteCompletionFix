ace9782955d35b4234630cc7eb4e131eb166f04a
Mads Kristensen <madskristensen@users.noreply.github.com>
2024-02-02 15:27:36 -0800

    Tightened up the rules

--- src/TypingCommandHandler.cs
+++ src/TypingCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.Composition;
 using Microsoft.VisualStudio.Commanding;
+using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
 using Microsoft.VisualStudio.Utilities;
@@ -21,33 +22,39 @@ namespace QuoteCompletionFix
                 return false;
             }
 
+            ITextSnapshot snapshot = args.TextView.TextSnapshot;
             var position = args.TextView.Caret.Position.BufferPosition.Position;
+            var next = position < snapshot.Length ? snapshot.GetText(position, 1)[0] : ' ';
+            var prev = position > 0 ? snapshot.GetText(position - 1, 1)[0] : ' ';
 
-            // Look at the next character
-            if (position < args.TextView.TextSnapshot.Length)
+            // Preserve current behavior when the caret is in between two quotation marks
+            if (prev == args.TypedChar && next == args.TypedChar)
             {
-                var next = args.TextView.TextSnapshot.GetText(position, 1)[0];
-
-                // If the next char is a letter or digit, insert the typed char and stop further processing resulting in auto-completion
-                if (char.IsLetterOrDigit(next))
-                {
-                    args.TextView.TextBuffer.Insert(position, args.TypedChar.ToString());
-                    return true;
-                }
+                return false;
             }
 
-            // Then look at the previous character
-            if (position > 0)
+            // If the next char is the typed char, then revert to existing behavior
+            // to support type-through (provisional text)
+            if (next == args.TypedChar)
             {
-                var prev = args.TextView.TextSnapshot.GetText(position - 1, 1)[0];
+                return false;
+            }
 
-                // If the next char is a letter or digit, but not a $ or @,
-                // insert the typed char and stop further processing resulting in auto-completion
-                if (char.IsLetterOrDigit(prev) && prev is not '$' and not '@')
-                {
-                    args.TextView.TextBuffer.Insert(position, args.TypedChar.ToString());
-                    return true;
-                }
+            // If the next char is a letter or digit,
+            // insert the typed char and stop further processing resulting in auto-completion
+            if (char.IsLetterOrDigit(next))
+            {
+                args.TextView.TextBuffer.Insert(position, args.TypedChar.ToString());
+                return true;
+            }
+
+            // If the next char is a letter or digit or a $ or @,
+            // insert the typed char and stop further processing resulting in auto-completion
+            if (char.IsLetterOrDigit(prev) || prev is '$' or '@')
+            {
+                args.TextView.TextBuffer.Insert(position, "" + args.TypedChar + args.TypedChar);
+                args.TextView.Caret.MoveToPreviousCaretPosition();
+                return true;
             }
 
             return false;
