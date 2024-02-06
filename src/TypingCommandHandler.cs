1457b888e8241fc00999e8f693bf8acb23d8da6a
Mads Kristensen <madskristensen@users.noreply.github.com>
2024-02-05 17:02:28 -0800

    Refactorings [release]

--- src/TypingCommandHandler.cs
+++ src/TypingCommandHandler.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.Commanding;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
+using Microsoft.VisualStudio.Text.Operations;
 using Microsoft.VisualStudio.Utilities;
 
 namespace QuoteCompletionFix
@@ -13,12 +14,16 @@ namespace QuoteCompletionFix
     [TextViewRole(PredefinedTextViewRoles.PrimaryDocument)]
     internal class TypingCommandHandler : ICommandHandler<TypeCharCommandArgs>
     {
-        public string DisplayName => throw new NotImplementedException();
+        public string DisplayName => nameof(TypingCommandHandler);
 
+        [Import]
+        public IEditorOperationsFactoryService EditorOperationsFactory { get; set; }
+
+        // Returning true means that further processing of the typed char should be stopped (we handled it).
+        // Returning false means that the built-in behavior should be used after this handler (we didn't handle it).
         public bool ExecuteCommand(TypeCharCommandArgs args, CommandExecutionContext executionContext)
         {
-            // Returning true means that further processing of the typed char should be stopped (we handled it).
-            // Returning false means that the built-in behavior should be used after this handler (we didn't handle it).
+            // Pass through if typed char isn't a quote
             if (args.TypedChar is not '"')
             {
                 return false;
@@ -29,37 +34,42 @@ namespace QuoteCompletionFix
             var next = position < snapshot.Length ? snapshot.GetText(position, 1)[0] : ' ';
             var prev = position > 0 ? snapshot.GetText(position - 1, 1)[0] : ' ';
 
-            // Rule #1: Preserve current behavior when the caret is in between two quotation marks
+            // Pass through to preserve current behavior when the caret is in between two quotation marks
             if (prev == args.TypedChar && next == args.TypedChar)
             {
                 return false;
             }
 
-            // Rule #2: If the next char is the typed char, then revert to existing behavior
-            // to support type-through (provisional text)
+            // Pass through if the next char is a quote to support type-through (provisional text)
+            // TODO: Check for existence of provisional text and only return false if it exists
             if (next == args.TypedChar)
             {
                 return false;
             }
 
-            // Rule #3: If the next char is a letter or digit,
-            // insert the typed char and stop further processing resulting in quote-completion
-            if (char.IsLetterOrDigit(next))
+            // We're in a state where we can run our rules
+            return ApplyRules(args, position, next, prev);
+        }
+
+        private bool ApplyRules(TypeCharCommandArgs args, int position, char next, char prev)
+        {
+            // Rule #1: If the next char is a letter or digit, don't auto-complete the quote
+            if (char.IsLetterOrDigit(next) || char.IsLetterOrDigit(prev))
             {
                 args.TextView.TextBuffer.Insert(position, args.TypedChar.ToString());
                 return true;
             }
 
-            // Rule #4: If the next char is a letter or digit or a $ or @,
-            // insert the typed char and stop further processing resulting in quote-completion
-            if (char.IsLetterOrDigit(prev) || prev is '$' or '@')
+            // Rule #2: If the previous char is a $ or @, auto-complete the quote
+            if (prev is '$' or '@')
             {
-                args.TextView.TextBuffer.Insert(position, "" + args.TypedChar + args.TypedChar);
+                args.TextView.TextBuffer.Insert(position, "" + args.TypedChar);
+                EditorOperationsFactory.GetEditorOperations(args.TextView).InsertProvisionalText("\"");
                 args.TextView.Caret.MoveToPreviousCaretPosition();
                 return true;
             }
 
-            return false;
+            return false; // No rules applied from this command handler. Pass through to built-in command handlers
         }
 
         public CommandState GetCommandState(TypeCharCommandArgs args)
